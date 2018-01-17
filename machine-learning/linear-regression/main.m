7057975d70ee90d51abf4f0da195ee77d4a38e0f
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-17 09:38:23 +0100

    Add part for loading and extracting test/training data
    -> training-test-threshold setup for 75%
    -> mayby will be extracted in some more customisable
       code later - for now, I left it static

--- machine-learning/linear-regression/main.m
+++ machine-learning/linear-regression/main.m
@@ -9,3 +9,25 @@
 
 % Clear current workspace.
   clear ; close all; clc
+
+% Load all given data as a matrix.
+  data = load('data.dat');
+
+% Extract training/test data for future model examination.
+%
+% training_test_threshold - represents the percentage ratio of training
+% data to test data.
+%
+% training_data - data that we use to train model.
+%
+% test_data - data that we use to test overall accuracy of the model
+%
+  training_test_threshold = 0.75;
+
+  training_data = data(
+    1:(floor(size(data, 1) * training_test_threshold)), :
+  );
+
+  test_data = data(
+    (ceil(size(data, 1) * training_test_threshold):size(data, 1)), :
+  );
