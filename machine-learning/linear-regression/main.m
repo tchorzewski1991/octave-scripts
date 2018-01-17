5485f9f7a2b2c3d1e0a5b7cf0434e90788d4d2ce
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-17 09:18:13 +0100

    Add a brief overview for linear regression

--- machine-learning/linear-regression/main.m
+++ machine-learning/linear-regression/main.m
@@ -1,4 +1,11 @@
 % Linear Regression
 
+% Linear Regression refers to model for modeling relationship
+% between dependent variable y (frequently called as a target) and
+% one, or more explanatory input variables called features.
+% See https://en.wikipedia.org/wiki/Linear_regression for more reference.
+
+% ======================================================================
+
 % Clear current workspace.
-clear ; close all; clc
+  clear ; close all; clc
