ad7a62cb163fe4619dba0204da88bc42400b1a65
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-23 20:17:11 +0100

    Add introduction note about regularized linear regression

---
+++ machine-learning/regularized-linear-regression/main.m
@@ -0,0 +1,13 @@
+% Regularized Linear Regression
+
+% Regularization -
+%   Is a process of adding additional terms to the cost function,
+%   often to introduce preference for simpler model. It turns out
+%   that linear dependency between input features and dependent
+%   output is really rare case in the 'real world' relationships.
+%   Much more 'right' solution to the problem could be using higher
+%   order polynomial model. It needs to be taken into consideration
+%   that process of selecting higher order polynomial could be
+%   involving and time consuming. It is not an easy task to pick in
+%   advance which parameter is less unlikely to be relevant. The
+%   'regularization' process refers to idea of shrinking them all.
