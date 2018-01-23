b9e36f53322194b4d36d8a449c1a1b60821e3fc0
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-23 20:22:22 +0100

    Add initial setup for working environment + load data file
    -> our data file is a set of relation between
       age of employee and achieved income in 1000's $

--- machine-learning/regularized-linear-regression/main.m
+++ machine-learning/regularized-linear-regression/main.m
@@ -11,3 +11,17 @@
 %   involving and time consuming. It is not an easy task to pick in
 %   advance which parameter is less unlikely to be relevant. The
 %   'regularization' process refers to idea of shrinking them all.
+
+%========================================================================
+
+  % Clears current workspace
+  clear; close all; clc;
+
+  % Loads all given data as a matrix.
+  % File 'data.dat' contains two columns:
+  %   - first column represents age of employee
+  %   - second column represents achieved income in 1000's $.
+
+  data = load('data.dat');
+
+% ========================================================================
