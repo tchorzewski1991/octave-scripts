d0c77402a07cacef9939fe2254b76d43e7f2a901
tchorzewski1991 <tchorzewski.rafal@gmail.com>
2018-01-24 12:29:43 +0100

    Describes all parts of featureNormalize() function

--- machine-learning/regularized-linear-regression/featureNormalize.m
+++ machine-learning/regularized-linear-regression/featureNormalize.m
@@ -1,9 +1,13 @@
 function [Xnorm, mu, sigma] = featureNormalize(X)
+  % Computes mean for given dataset.
   mu = mean(X);
 
+  % Applies binary minus() function to each element in dataset.
   Xnorm = bsxfun(@minus, X, mu);
 
+  % Computes standard deviations for given dataset.
   sigma = std(Xnorm);
 
+  % Applies binary rdivide() function to each element in dataset.
   Xnorm = bsxfun(@rdivide, Xnorm, sigma);
 end
